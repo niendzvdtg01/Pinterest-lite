05ab33de0579ba560c600af729b13619edfb5217
niendzvdtg01 <duynien34@gmail.com>
2026-01-13 22:13:50 +0700

    complete: favourite service

--- src/Components/Favourite/UI/Mansory.jsx
+++ src/Components/Favourite/UI/Mansory.jsx
@@ -1,11 +1,32 @@
 import { useFavourite } from "../API/useFavourite"
-
+import '../favourite.scss'
 export default function Mansory() {
     const context = useFavourite();
-    console.log(context);
+    const data = context.favourites;
     return (
         <>
+            <div className="mt-3 w-100 p-3">
+                < div className="masonry-layout" >
+                    {
+                        data.map((photo) => (
+                            <div key={photo.id} className="masonry-item">
+                                <a >
+                                    <div class="overlay">
+                                    </div>
 
+                                    <img
+                                        src={photo.imageUrl}
+                                        alt={"unsplash picture"}
+                                    />
+                                </a>
+                                <p className="author">
+                                    {photo.title}
+                                </p>
+                            </div>
+                        ))
+                    }
+                </div >
+            </div>
         </>
     )
 }
